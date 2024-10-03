82f79fa53813bd700108e999e0280b8b99357b9c
ayeshamukta <ayeshamukta0000@gmail.com>
2024-10-04 01:02:13 +0600

    implemented add money feature

--- js/home.js
+++ js/home.js
@@ -1,1 +1,22 @@
+document.getElementById('btn-add-money').addEventListener("click",function(event)
+{
+    event.preventDefault();
+    const addAmount = document.getElementById('addInput').value;
+    const pin = document.getElementById('check-pin').value;
+    const currentBalance = document.getElementById('current-amount').innerText;
+    
+    
+    if(pin ==='1234')
+    {
+        const totalBalance = parseFloat(addAmount) + parseFloat(currentBalance);
+        document.getElementById('current-amount').innerText = totalBalance;
+    }
+    else
+    {
+        alert('Incorrect Pin');
+    } 
+    
+    
+    
+})
 
